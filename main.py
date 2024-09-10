841f83e84d67bad994ccf9b95b67b86e38ade9d0
Trent O'Dell <165042958+Lamurettee@users.noreply.github.com>
2024-09-10 16:26:15 -0700

    init 2
    init 2

--- main.py
+++ main.py
@@ -1,153 +1,182 @@
-import sys, csv
-
+from notion_client import Client
+from notion_client.errors import APIResponseError
 from src.util.load_json import load_data, _Creature
+from src.util.children_func import (
+    add_columns,
+    add_bulleted_list,
+    add_numbered_list,
+    add_paragraph,
+    add_section_heading,
+    add_table,
+    add_divider,
+)
 
 # py-dm-tk call main program window.
 NAME = "Notion 5E Database Builder"
 DESCRIPTION = "Convert json data into a Notion CSV and MD files"
 VERSION = "0.0.1"
 DATA_DIRECTORY = "src/data"
+notion = Client(auth="secret_8fDt3EJbAlcH0tWh6u26x5RfTBVkfHZYW9nUUN6K1gU")
+database_id = "997bb9bb0bc048ecb6c06523c4af66aa"  # type: ignore
+print("Database created with ID:", database_id)
 
 
-def parsing_monster_data(data):
-
-    def table_data_name():
-        creature_list.append(f"{x.name}")
-
-    def table_data_challenge_rating():
-        creature_list.append(x.challenge_rating)
-
-    def table_data_creature_type():
-        creature_list.append(f"{x.type.capitalize()}")
-
-    def table_data_creature_hp():
-        creature_list.append(x.hit_points)
-
-    def table_data_languages():
-        languages = x.languages
-        lang = languages.split(", ")
-        merg_lang = "\n".join(list(lang))
-        creature_list.append(f"{merg_lang}")
-
-    def table_data_movement():
-        parsed_speed = [
-            f"{Movement.capitalize()}: {Distance}"
-            for Movement, Distance in x.speed.items()
-        ]
-
-        merged_speed = "\n".join(list(parsed_speed))
-        creature_list.append(f"{merged_speed}")
-
-    def table_data_resistances():
-        parsed_dam_res = list(x.damage_resistances)
-        if parsed_dam_res:
-            parsed_dam_res = "\n".join([x.capitalize() for x in parsed_dam_res])
-            creature_list.append(f"{parsed_dam_res}")
-        else:
-            parsed_dam_res = ""
-            creature_list.append(f"{parsed_dam_res}")
-
-    def table_data_immunities():
-        parsed_dam_imun = list(x.damage_immunities)
-        if parsed_dam_imun:
-            parsed_dam_imun = "\n".join([x.capitalize() for x in parsed_dam_imun])
-            creature_list.append(f"{parsed_dam_imun}")
-        else:
-            parsed_dam_imun = ""
-            creature_list.append(f"{parsed_dam_imun}")
-
-    def table_data_spells():
-        parsed_spell = ""
-        if x.special_abilities:
-            for spell in x.special_abilities:
-                if spell["name"].startswith("Spellcasting"):
-
-                    parsed_spell = "\n".join(
-                        [spell["name"] for spell in spell["spellcasting"]["spells"]]
-                    )
-
-        creature_list.append(f"{parsed_spell}")
-
-    def table_data_saving_throws():
-        saving_lines = []
-        for val in x.proficiencies:
-            name = val["proficiency"]["name"]
-            if name.startswith("Saving Throw"):
-                name = name.removeprefix("Saving Throw: ")
-                saving_lines.append(f"{name}: {val['value']}")
-
-        final_saving_line = "\n".join(saving_lines)
-        creature_list.append(final_saving_line if saving_lines else "")
-
-    def table_data_skills():
-        skill_lines = []
-        for val in x.proficiencies:
-            name = val["proficiency"]["name"]
-            if name.startswith("Skill"):
-                skill_lines.append(f"{name[7:]}: {val['value']}")
-
-        final_skill_line = "\n".join(skill_lines)
-        creature_list.append(final_skill_line if skill_lines else "")
-
-    table_data_dict_functions = {
-        "Creature Name": {"column": "0", "function": table_data_name},
-        "CR": {"column": "1", "function": table_data_challenge_rating},
-        "Type": {"column": "2", "function": table_data_creature_type},
-        "Hit Points": {"column": "3", "function": table_data_creature_hp},
-        "Movement": {"column": "4", "function": table_data_movement},
-        "Languages": {"column": "5", "function": table_data_languages},
-        "Spells": {"column": "6", "function": table_data_spells},
-        "Saving Throws": {"column": "7", "function": table_data_saving_throws},
-        "Skills": {"column": "8", "function": table_data_skills},
-        "Resistances": {"column": "9", "function": table_data_resistances},
-        "Immunities": {"column": "10", "function": table_data_immunities},
-    }
-
-    header_data = list(table_data_dict_functions.keys())
-
-    # Iterate over the creatures model and build the table data
-    processed_data = []
-    for x in data:
-        x = _Creature(**x)
-        creature_list = []
-        # Iterate over the keys in the dictionary
-        for key in table_data_dict_functions:
-            # Get the dictionary associated with the key
-            table_data_dict_functions[key]["function"]()
-
-        processed_data.append(creature_list)
-
-    packaged_data = [header_data + processed_data]
-
-    return packaged_data
-
-
-# Declare basic window variables and execute.
-def main():
-    creature_data = load_data(DATA_DIRECTORY)
-    table = parsing_monster_data(creature_data)
-    # print(table)
-    flattened_data = [
-        item for row in table for item in (row if isinstance(row, list) else [row])
+def ability_modifier(number):
+    # Calculating abilit score modifiers
+    ability_table = [
+        ("1", "-5"),
+        ("2-3", "-4"),
+        ("4-5", "-3"),
+        ("6-7", "-2"),
+        ("8-9", "-1"),
+        ("10-11", "0"),
+        ("12-13", "+1"),
+        ("14-15", "+2"),
+        ("16-17", "+3"),
+        ("18-19", "+4"),
+        ("20-21", "+5"),
+        ("22-23", "+6"),
+        ("24-25", "+7"),
+        ("26-27", "+8"),
+        ("28-29", "+9"),
+        ("30", "+10"),
     ]
-    with open("creatures.csv", "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
-        # Adjust per header
-        writer.writerow(flattened_data[:11])  # Write the header row
-        writer.writerows(flattened_data[11:])  # Write the data rows
-    """
-    for creature in creature_data:
 
-        print(creature["name"])
-        print(creature["size"])
-        print(creature["type"].capitalize())
-        print(creature["alignment"].capitalize())
+    for range, value in ability_table:
+        if "-" in range:
+            start, end = map(int, range.split("-"))
+            if start <= number <= end:
+                return value
+        elif int(range) == number:
+            return value
 
-        armor_details = [
-            f"{armor['type']} - {armor['value']}" for armor in creature["armor_class"]
-        ]
-        print(f"{armor_details}")
-    """
+
+def build_children_blocks(monster):
+    # =======================================================
+    # ==  All of the pre work is here for string manipulation
+    # =======================================================
+
+    # monster = _Creature(**x)
+
+    # ==  Parsing speed
+    parsed_speed = [
+        f"{Movement}: {Distance}" for Movement, Distance in monster.speed.items()
+    ]
+    parsed_speed = " - ".join([x.capitalize() for x in parsed_speed])
+
+    # ==  Armor parsing
+    parsed_armor = []
+    for armor in monster.armor_class:
+        if "spell" in armor:
+            parsed_armor.append(
+                f"{armor['type'].capitalize()} {armor['value']} : {armor['spell']['name']}"
+            )
+        elif "condition" in armor:
+            parsed_armor.append(
+                f"{armor['type'].capitalize()} {armor['value']} : {armor['condition']['name']}"
+            )
+        elif "armor" in armor:
+            for x in armor["armor"]:
+                parsed_armor.append(
+                    f"{armor['type'].capitalize()} {armor['value']} : {x['name']}"
+                )
+        else:
+            parsed_armor.append(f"{armor['type'].capitalize()} {armor['value']}")
+    parsed_armor = " - ".join(parsed_armor)
+
+    # ==  Building list for basic info
+    basic_info_list = [
+        f"Armor Class : {parsed_armor}",
+        f"Hit Points : {monster.hit_points} ( {monster.hit_points_roll} )",
+        f"Speed : {parsed_speed}",
+    ]
+    # =======================================================
+    # ==  End of pre work
+    # =======================================================
+    # ==
+    # =======================================================
+    # ==  This is all of the building of the api call for
+    # ==  the markdown body
+    # =======================================================
+
+    # ==  Initializing all the blank list
+    left_column_content = []
+    right_column_content = []
+    children = []
+
+    # ==  Adding header at the top
+    add_section_heading(children, f"{monster.name}", level=1)
+    add_paragraph(
+        children,
+        f"{monster.size.capitalize()} {monster.type.capitalize()} , {monster.alignment}",
+    )
+    add_divider(children)
+
+    # ==  Adding basic info
+    add_bulleted_list(right_column_content, basic_info_list)
+    add_divider(children)
+
+    # == Stats Section
+    add_section_heading(left_column_content, "Stats", level=2)
+    stats_table_headers = ["STR", "Value"]
+    stats_table_rows = [
+        ["Strength", (f"{monster.strength}")],
+        ["Dexterity", (f"{monster.dexterity}")],
+        ["Constitution", (f"{monster.constitution}")],
+        ["Intelligence", (f"{monster.intelligence}")],
+        ["Wisdom", (f"{monster.wisdom}")],
+        ["Charisma", (f"{monster.charisma}")],
+    ]
+    add_table(left_column_content, stats_table_headers, stats_table_rows)
+    add_divider(children)
+
+    # Add columns to the children
+    add_columns(children, left_column_content, right_column_content)
+
+    return children
+
+
+def main():
+    # Get Monster Data
+    creature_data = load_data(DATA_DIRECTORY)
+
+    # Iterates through the monster JSON
+    for x in creature_data:
+
+        # Makes the creature as a data class
+        monster = _Creature(**x)
+        if monster.name == "Aboleth":
+            try:
+
+                children = build_children_blocks(monster)
+
+                response = notion.pages.create(
+                    parent={"database_id": database_id},
+                    properties={
+                        "Name": {
+                            "title": [
+                                {
+                                    "type": "text",
+                                    "text": {"content": monster.name.capitalize()},
+                                }
+                            ]
+                        },
+                        "Size": {"select": {"name": monster.size.capitalize()}},
+                        "Type": {"select": {"name": monster.type.capitalize()}},
+                        "CR": {"number": monster.challenge_rating},
+                        "Hit Points": {"number": monster.hit_points},
+                        "Hit Points": {"number": monster.hit_points},
+                        "Movement Type": {
+                            "multi_select": [
+                                {"name": mt.capitalize()} for mt in monster.speed
+                            ]
+                        },
+                    },
+                    children=children,
+                )
+                print(f"Page created for {monster.name} with ID: {response['id']}")  # type: ignore
+            except APIResponseError as e:
+                print(f"An API error occurred: {e}")
 
 
 if __name__ == "__main__":
